50645ab9f2544ceeb3580933dfb08fb265940eb3
FEITENG <wxfeit@163.com>
2024-08-01 01:12:24 +0800

    test for QJoysticks

--- main.cpp
+++ main.cpp
@@ -2,10 +2,27 @@
 
 #include <QApplication>
 
+#include "QJoysticks.h"
+#include <QDebug>
+
+#ifdef Q_OS_WIN
+#   ifdef main
+#      undef main
+#   endif
+#endif
+
 int main(int argc, char *argv[])
 {
-  QApplication a(argc, argv);
-  MainWindow w;
-  w.show();
-  return a.exec();
+    QApplication a(argc, argv);
+    MainWindow w;
+
+    // QJoysticks* instance = QJoysticks::getInstance();
+    // instance->setVirtualJoystickEnabled(false);
+
+    // instance->updateInterfaces();
+    // qDebug()<<instance->nonBlacklistedCount();
+    // qDebug()<<instance->deviceNames();
+
+    w.show();
+    return a.exec();
 }
--- mainwindow.h
+++ mainwindow.h
@@ -4,18 +4,24 @@
 #include <QMainWindow>
 
 QT_BEGIN_NAMESPACE
-namespace Ui { class MainWindow; }
+namespace Ui
+{
+    class MainWindow;
+}
 QT_END_NAMESPACE
 
 class MainWindow : public QMainWindow
 {
-  Q_OBJECT
+        Q_OBJECT
 
-public:
-  MainWindow(QWidget *parent = nullptr);
-  ~MainWindow();
+    public:
+        MainWindow(QWidget *parent = nullptr);
+        ~MainWindow();
 
-private:
-  Ui::MainWindow *ui;
+    private:
+        Ui::MainWindow *ui;
+
+    public slots:
+        void updateJS();
 };
 #endif // MAINWINDOW_H
